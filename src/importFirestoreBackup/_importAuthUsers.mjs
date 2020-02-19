e403355ccc9af4053df8bc86c941f59673f1b2fc
Pascal <pascal@luther.ch>
2020-02-19 11:30:41 +0100

    fixed import of auth users

--- src/importFirestoreBackup/_importAuthUsers.mjs
+++ src/importFirestoreBackup/_importAuthUsers.mjs
@@ -1,12 +1,13 @@
 import { inq } from "../_helpers/inquirerWrapper.mjs";
 import asyncExec from "../_helpers/asyncExec.mjs";
 
-export default async function({ projectId }) {
+export default async function({ projectId, backupId }) {
   const prompts = global.prompts;
+  const filePath = `./firebackups/${projectId}/${backupId}/AllAuthUsers.json`;
   try {
     const answers = await inq.prompt([prompts.hashKey, prompts.saltSeparator]);
     console.info(`💬 Importing auth user into ${projectId}...`);
-    const cliCmd = `firebase use ${projectId} && firebase auth:import ./${projectId}/AllAuthUsers.json --hash-algo=scrypt --rounds=8 --mem-cost=14 --hash-key=${answers.hashKey} --salt-separator=${answers.saltSeparator}`;
+    const cliCmd = `firebase use ${projectId} && firebase auth:import ${filePath} --hash-algo=scrypt --rounds=8 --mem-cost=14 --hash-key=${answers.hashKey} --salt-separator=${answers.saltSeparator}`;
     await asyncExec(cliCmd);
     console.info(`✅ Imported Auth User into ${projectId}.`);
   } catch (e) {
